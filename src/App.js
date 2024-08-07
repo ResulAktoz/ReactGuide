f21f2086f0ab5f65a20b7ab651a9498f142c26e9
Resul Aktoz <aktozresul@gmail.com>
2024-08-07 14:41:02 +0300

    readme and api url changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
 import logo from "./logo.svg"
 
 function App() {
+
+useEffect(() => {
+  if(process.env.NODE_ENV == 'production'){
+    }
+}, [])
+
   return (
     <div className="App">
-    <img src="/logo192.png" alt="" />
-    <img src={logo} alt="" />
-    </div>
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+        <img src="/logo192.png" alt="" />
+        <img src={logo} alt="" />
+        </>
+      )}
+      </div>
   );
 }
 
