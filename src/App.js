5709e1a738991dee828db4601b314b19decc1888
Resul Aktoz <aktozresul@gmail.com>
2024-08-07 16:38:38 +0300

    styled components examples added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { useEffect } from "react";
 import logo from "./logo.svg"
+import Test from "./Test"
+import styles from "./App.module.css"
+import{Title} from "./components"
 
 function App() {
+
+/*useEffect (() => {
+  console.log(styles)
+})*/
 
 useEffect(() => {
   if(process.env.NODE_ENV == 'production'){
@@ -9,11 +16,13 @@ useEffect(() => {
 }, [])
 
   return (
-    <div className="App">
-      <h3>{process.env.NODE_ENV}</h3>
+    <div className={styles.App}>
+      <Title>{process.env.NODE_ENV}</Title>
+      <Title theme = 'dark'>{process.env.NODE_ENV}</Title>
       <p>
         {process.env.REACT_APP_API_URL}
       </p>
+      <Test />
       {process.env.NODE_ENV === 'production' && (
         <>
         <img src="/logo192.png" alt="" />
